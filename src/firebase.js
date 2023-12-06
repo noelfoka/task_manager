b38115a5b776237bac21cd0dd1fa817c6bc172c6
noelfoka <nomgnenoel@gmail.com>
2023-12-06 04:05:43 +0100

    add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3WimzyUV4Rp7T_s431bufk4QMeDi9w2M",
+    authDomain: "task-manager-fd27d.firebaseapp.com",
+    projectId: "task-manager-fd27d",
+    storageBucket: "task-manager-fd27d.appspot.com",
+    messagingSenderId: "625028214224",
+    appId: "1:625028214224:web:4a09fc1c748b6aba0f5dd5",
+    measurementId: "G-63VRMP4RQQ"
+  };
